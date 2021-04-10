9b066e53d2fbfe2fa6704717fd5f68b6edba623d
pandanoir <moon_in_france@yahoo.co.jp>
2021-04-10 14:02:09 +0900

    Split files

---
+++ src/utils/useInput.ts
@@ -0,0 +1,15 @@
+import { useState, useCallback, ChangeEvent } from 'react';
+
+export const useInput = (initialValue = '') => {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => setValue(e.currentTarget.value),
+    []
+  );
+
+  return {
+    value,
+    onChange,
+  } as const;
+};
