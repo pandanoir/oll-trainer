2cdeeaed830093f2142a8079cc6938e1d6db1b61
pandanoir <moon_in_france@yahoo.co.jp>
2021-05-09 14:39:39 +0900

    Add clickAway

---
+++ src/utils/clickAway.ts
@@ -0,0 +1,13 @@
+export const clickAway = (el: HTMLElement, onAway: () => void) => {
+  const onClick = (ev: MouseEvent) => {
+    const path = ev.composedPath();
+    // 含まれてる = 内側 = 何もしない
+    if (path.includes(el)) return;
+    onAway();
+  };
+
+  document.addEventListener('click', onClick);
+  return () => {
+    document.removeEventListener('click', onClick);
+  };
+};
