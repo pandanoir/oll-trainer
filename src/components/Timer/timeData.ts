5072edfd81e4da39deef9c280a5dc0b36861e596
pandanoir <moon_in_france@yahoo.co.jp>
2021-05-09 14:40:05 +0900

    Add date property to TimeData

--- src/components/Timer/timeData.ts
+++ src/components/Timer/timeData.ts
@@ -6,5 +6,40 @@ export type TimeData = {
   penalty?: boolean;
   isDNF?: boolean;
   scramble: string;
+  date: number; // millisec
 };
 export const DNF = 'dnf';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isUnknownObject = (item: any): item is Record<PropertyKey, unknown> => {
+  return typeof item === 'object' && item !== null;
+};
+export const fromCsTimer = (json: unknown) => {
+  if (!isUnknownObject(json)) {
+    throw new Error('invalid JSON given');
+  }
+  return Object.keys(json).reduce((acc: TimeData[][], key) => {
+    if (!key.startsWith('session')) {
+      return acc;
+    }
+    const arr = json[key];
+    if (!Array.isArray(arr)) {
+      throw new Error('invalid JSON given');
+    }
+    return [
+      ...acc,
+      arr.map((item: unknown) => {
+        if (!Array.isArray(item)) {
+          throw new Error('invalid JSON given');
+        }
+        return {
+          penalty: item[0][0] === 2000,
+          isDNF: item[0][0] === -1,
+          scramble: item[1],
+          date: item[3],
+          time: item[0][1],
+        };
+      }),
+    ];
+  }, []);
+};
