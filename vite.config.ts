3039b64792d436341c9de6de488624b8e5f36572
pandanoir <moon_in_france@yahoo.co.jp>
2021-05-09 14:12:17 +0900

    Install twin.macro and styled-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import macrosPlugin from 'vite-plugin-babel-macros';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), macrosPlugin()],
   esbuild: {
     jsxInject: `import React from 'react'`,
+  },
+  define: {
+    'process.platform': JSON.stringify('win32'),
+    'process.env': {},
   },
   base: '/oll/',
 });
