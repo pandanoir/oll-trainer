95fbba3898f5b945ba6d6afda9b4029a85ac2c6b
pandanoir <moon_in_france@yahoo.co.jp>
2021-05-09 14:17:33 +0900

    Remove buttons in record list item

--- src/components/Timer/Times.tsx
+++ src/components/Timer/Times.tsx
@@ -3,34 +3,11 @@ import { calcAo } from '../../utils/calcAo';
 import { showTime } from './showTime';
 import { TimeData, DNF } from './timeData';
 
-const Button: VFC<
-  PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>
-> = ({ children, className = '', ...props }) => {
-  return (
-    <button
-      {...props}
-      className={`border border-gray-400 rounded-sm px-1 ${className}`}
-    >
-      {children}
-    </button>
-  );
-};
-
-const Record: VFC<{
+const SimpleRecord: VFC<{
   index: number;
   time: TimeData;
-  changeToDNF: () => void;
-  undoDNF: () => void;
-  imposePenalty: () => void;
-  undoPenalty: () => void;
-}> = ({
-  index,
-  time: { time, penalty, isDNF },
-  changeToDNF,
-  undoDNF,
-  imposePenalty,
-  undoPenalty,
-}) => {
+  onClick?: () => void;
+}> = ({ index, time: { time, penalty, isDNF }, onClick }) => {
   const record = isDNF
     ? 'DNF'
     : penalty
@@ -38,23 +15,8 @@ const Record: VFC<{
     : showTime(time);
 
   return (
-    <span>
-      {index + 1}. {record}{' '}
-      {isDNF ? (
-        <span>
-          <Button onClick={undoDNF}>undo DNF</Button>
-        </span>
-      ) : penalty ? (
-        <span className="inline-grid gap-1 grid-cols-2">
-          <Button onClick={undoPenalty}>-2</Button>
-          <Button onClick={changeToDNF}>DNF</Button>
-        </span>
-      ) : (
-        <span className="inline-grid gap-1 grid-cols-2">
-          <Button onClick={imposePenalty}>+2</Button>
-          <Button onClick={changeToDNF}>DNF</Button>
-        </span>
-      )}
+    <span onClick={onClick} className="cursor-pointer">
+      {index + 1}. {record}
     </span>
   );
 };
@@ -68,38 +30,22 @@ export const Times: VFC<{
   const ao5List = useMemo(() => calcAo(5, times), [times]);
   const ao12List = useMemo(() => calcAo(12, times), [times]);
   return (
-    <ul>
+    <ul className="flex flex-col-reverse">
       {times.map((time, index) => {
         const ao5 = ao5List[index];
         const ao12 = ao12List[index];
         return (
-          <li key={index} className="grid grid-cols-3">
-            <Record
+          <li key={time.date} className="grid grid-cols-3">
+            <SimpleRecord
               index={index}
               time={time}
-              changeToDNF={() => changeToDNF(index)}
-              undoDNF={() => undoDNF(index)}
-              imposePenalty={() => imposePenalty(index)}
-              undoPenalty={() => undoPenalty(index)}
+              onClick={() => {
+                setSelectedIndex(index);
+                openModal();
+              }}
             />
-            {ao5 ? (
-              ao5 === DNF ? (
-                <span>DNF</span>
-              ) : (
-                <span>{showTime(ao5)}</span>
-              )
-            ) : (
-              <span>-</span>
-            )}
-            {ao12 ? (
-              ao12 === DNF ? (
-                <span>DNF</span>
-              ) : (
-                <span>{showTime(ao12)}</span>
-              )
-            ) : (
-              <span>-</span>
-            )}
+            <span>{ao5 ? (ao5 === DNF ? 'DNF' : showTime(ao5)) : '-'}</span>
+            <span>{ao12 ? (ao12 === DNF ? 'DNF' : showTime(ao12)) : '-'}</span>
           </li>
         );
       })}
