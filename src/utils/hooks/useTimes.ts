86752e54fc4f7d8a15eb59c9df4a9fd90aa507a0
pandanoir <moon_in_france@yahoo.co.jp>
2021-05-09 15:36:39 +0900

    Refactor useTimes

--- src/utils/hooks/useTimes.ts
+++ src/utils/hooks/useTimes.ts
@@ -1,32 +1,56 @@
 import { TimeData } from '../../components/Timer/timeData';
 import { useLocalStorage } from './useLocalStorage';
 import { storagePrefix } from '../../constants';
+import { useCallback } from 'react';
 
 export const useTimes = (initialTimes: TimeData[] = []) => {
   const [times, setTimes] = useLocalStorage(
     `${storagePrefix}-times`,
     initialTimes
   );
-  const updateItem = (index: number, newItem: TimeData) => (
-    times: TimeData[]
-  ) => [...times.slice(0, index), newItem, ...times.slice(index + 1)];
+  const updateItem = (
+    index: number,
+    convert: (prevValue: TimeData) => TimeData
+  ) => (times: TimeData[]) => [
+    ...times.slice(0, index),
+    convert(times[index]),
+    ...times.slice(index + 1),
+  ];
 
-  const changeToDNF = (index: number) => {
-    setTimes(updateItem(index, { ...times[index], isDNF: true }));
-  };
-  const undoDNF = (index: number) => {
-    setTimes(updateItem(index, { ...times[index], isDNF: false }));
-  };
-  const imposePenalty = (index: number) => {
-    setTimes(updateItem(index, { ...times[index], penalty: true }));
-  };
-  const undoPenalty = (index: number) => {
-    setTimes(updateItem(index, { ...times[index], penalty: false }));
-  };
-  const deleteRecord = (index: number) => {
-    setTimes((times) => [...times.slice(0, index), ...times.slice(index + 1)]);
-    return times[index];
-  };
+  const changeToDNF = useCallback(
+    (index: number) => {
+      setTimes(updateItem(index, (prev) => ({ ...prev, isDNF: true })));
+    },
+    [setTimes]
+  );
+  const undoDNF = useCallback(
+    (index: number) => {
+      setTimes(updateItem(index, (prev) => ({ ...prev, isDNF: false })));
+    },
+    [setTimes]
+  );
+  const imposePenalty = useCallback(
+    (index: number) => {
+      setTimes(updateItem(index, (prev) => ({ ...prev, penalty: true })));
+    },
+    [setTimes]
+  );
+  const undoPenalty = useCallback(
+    (index: number) => {
+      setTimes(updateItem(index, (prev) => ({ ...prev, penalty: false })));
+    },
+    [setTimes]
+  );
+  const deleteRecord = useCallback(
+    (index: number) => {
+      setTimes((times) => [
+        ...times.slice(0, index),
+        ...times.slice(index + 1),
+      ]);
+      return times[index];
+    },
+    [times, setTimes]
+  );
   const insertRecord = (index: number, record: TimeData) => {
     setTimes((times) => [
       ...times.slice(0, index),
