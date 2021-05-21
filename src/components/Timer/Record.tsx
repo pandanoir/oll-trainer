02c8162e20f64b65d062684f0ad95acd7d857131
pandanoir <moon_in_france@yahoo.co.jp>
2021-05-22 00:24:40 +0900

    Add graph function

--- src/components/Timer/Record.tsx
+++ src/components/Timer/Record.tsx
@@ -1,8 +1,81 @@
-import { Dispatch, SetStateAction, useRef, useState } from 'react';
+import {
+  ButtonHTMLAttributes,
+  Dispatch,
+  SetStateAction,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import {
+  ResponsiveContainer,
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Line,
+} from 'recharts';
+import {
+  faAngleLeft,
+  faAngleRight,
+  faPlus,
+  faChartBar,
+  faAngleDown,
+  faAngleUp,
+  faServer,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import tw from 'twin.macro';
+import { calcAo } from '../../utils/calcAo';
 import { RecordListHeader } from './RecordListHeader';
 import { TimeData, SessionData } from './timeData';
 import { Times } from './Times';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
+const IconButton = ({
+  icon,
+  ...props
+}: { icon: IconProp } & ButtonHTMLAttributes<HTMLButtonElement>) => (
+  <button {...props}>
+    <FontAwesomeIcon icon={icon} />
+  </button>
+);
+const AngleLeftButton = ({
+  onClick,
+  disabled = false,
+}: {
+  onClick: () => void;
+  disabled?: boolean;
+}) => (
+  <button
+    css={[tw`px-3`, disabled ? tw`text-gray-400` : '']}
+    disabled={disabled}
+    onClick={onClick}
+  >
+    <FontAwesomeIcon icon={faAngleLeft} />
+  </button>
+);
+const AngleRightButton = ({
+  onClick,
+  disabled = false,
+}: {
+  onClick: () => void;
+  disabled?: boolean;
+}) => (
+  <button
+    css={[tw`px-3`, disabled ? tw`text-gray-400` : '']}
+    disabled={disabled}
+    onClick={onClick}
+  >
+    <FontAwesomeIcon icon={faAngleRight} />
+  </button>
+);
+const PlusButton = ({ onClick }: { onClick: () => void }) => (
+  <button tw="px-1.5 text-white bg-gray-700 rounded" onClick={onClick}>
+    <FontAwesomeIcon icon={faPlus} />
+  </button>
+);
 export const Record = ({
   times,
   changeToDNF,
@@ -34,41 +107,147 @@ export const Record = ({
 }) => {
   const recordListRef = useRef<HTMLDivElement>(null);
   const [opensRecordList, setOpensRecordList] = useState(true);
+  const ao5List = useMemo(() => calcAo(5, times), [times]);
+  const ao12List = useMemo(() => calcAo(12, times), [times]);
+
+  const [showsGraph, setShowsGraph] = useState(false);
+
+  const resetScroll = () => {
+    if (recordListRef.current) {
+      recordListRef.current.scrollTo(0, 0);
+    }
+  };
   return (
     <div
-      className={`${
-        opensRecordList ? `h-32 md:h-64 overflow-y-scroll` : `h-6`
-      } transition-all duration-300`}
+      css={[
+        opensRecordList
+          ? [tw`h-32 md:h-64`, showsGraph ? '' : tw`overflow-y-scroll`]
+          : [tw`h-6`],
+        tw`flex`,
+        tw`flex-col`,
+        tw`transition-all`,
+        tw`duration-300`,
+      ]}
       ref={recordListRef}
     >
       <RecordListHeader
-        sessionIndex={sessionIndex}
-        setSessionIndex={setSessionIndex}
-        changeSessionName={changeSessionName}
-        sessions={sessions}
-        resetScroll={() => {
-          if (recordListRef.current) {
-            recordListRef.current.scrollTo(0, 0);
-          }
-        }}
-        open={opensRecordList}
-        onOpenButtonClick={() => setOpensRecordList((open) => !open)}
-        addSession={() => {
-          addSession();
-          if (sessions.length - 1 === sessionIndex) {
-            setSessionIndex(sessions.length);
-          }
-        }}
+        left={
+          <div>
+            <AngleLeftButton
+              disabled={sessionIndex <= 0}
+              onClick={() => {
+                setSessionIndex((index) => index - 1);
+                resetScroll();
+              }}
+            />
+            <input
+              value={sessions[sessionIndex].name}
+              onChange={({ target: { value } }) => changeSessionName(value)}
+            />
+            <AngleRightButton
+              disabled={sessionIndex + 1 >= sessions.length}
+              onClick={() => {
+                setSessionIndex((index) => index + 1);
+                resetScroll();
+              }}
+            />
+            <PlusButton
+              onClick={() => {
+                addSession();
+                if (sessions.length - 1 === sessionIndex) {
+                  setSessionIndex(sessions.length);
+                }
+              }}
+            />
+          </div>
+        }
+        right={
+          <div>
+            {showsGraph ? (
+              <IconButton
+                onClick={() => setShowsGraph(false)}
+                icon={faServer}
+              />
+            ) : (
+              <IconButton
+                onClick={() => setShowsGraph(true)}
+                icon={faChartBar}
+              />
+            )}
+            <IconButton
+              tw="px-2"
+              onClick={() => setOpensRecordList((open) => !open)}
+              icon={opensRecordList ? faAngleDown : faAngleUp}
+            />
+          </div>
+        }
       />
-      <Times
-        times={times}
-        changeToDNF={changeToDNF}
-        imposePenalty={imposePenalty}
-        undoDNF={undoDNF}
-        undoPenalty={undoPenalty}
-        deleteRecord={deleteRecord}
-        insertRecord={insertRecord}
-      />
+      <div tw="flex-1">
+        {showsGraph ? (
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              width={500}
+              height={300}
+              data={times.map(({ time }, index) => {
+                const ao5 = ao5List[index],
+                  ao12 = ao12List[index];
+                return {
+                  name: index + 1,
+                  time: time / 1000,
+                  ao5: typeof ao5 === 'number' ? ao5 / 1000 : null,
+                  ao12: typeof ao12 === 'number' ? ao12 / 1000 : null,
+                };
+              })}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <Legend verticalAlign="top" />
+              <XAxis dataKey="name" />
+              <YAxis dataKey="time" />
+              <Legend />
+              <Tooltip />
+              <Line
+                name="time"
+                type="linear"
+                dataKey="time"
+                stroke="#000"
+                dot={false}
+              />
+              <Line
+                name="ao5"
+                type="linear"
+                dataKey="ao5"
+                stroke="#3b82f6"
+                dot={false}
+                strokeDasharray="4"
+              />
+              <Line
+                name="ao5"
+                type="linear"
+                dataKey="ao12"
+                stroke="#db2777"
+                dot={false}
+                strokeDasharray="2 2"
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        ) : (
+          <Times
+            times={times}
+            changeToDNF={changeToDNF}
+            imposePenalty={imposePenalty}
+            undoDNF={undoDNF}
+            undoPenalty={undoPenalty}
+            deleteRecord={deleteRecord}
+            insertRecord={insertRecord}
+          />
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Timer/RecordListHeader.tsx
+++ src/components/Timer/RecordListHeader.tsx
@@ -1,74 +1,15 @@
-import { Dispatch, SetStateAction } from 'react';
-import {
-  faAngleDown,
-  faAngleLeft,
-  faAngleRight,
-  faAngleUp,
-  faPlus,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { SessionData } from './timeData';
-
+import 'twin.macro';
 export const RecordListHeader = ({
-  sessionIndex,
-  setSessionIndex,
-  changeSessionName,
-  resetScroll,
-  sessions,
-  open,
-  onOpenButtonClick,
-  addSession,
+  left,
+  right,
 }: {
-  sessionIndex: number;
-  setSessionIndex: Dispatch<SetStateAction<number>>;
-  changeSessionName: (name: string) => void;
-  resetScroll: () => void;
-  sessions: SessionData[];
-  open: boolean;
-  onOpenButtonClick: () => void;
-  addSession: () => void;
+  left: JSX.Element;
+  right: JSX.Element;
 }) => {
   return (
-    <div className="sticky top-0 bg-white flex justify-between">
-      <div>
-        <button
-          className={`px-3 ${sessionIndex > 0 ? '' : 'text-gray-400'}`}
-          disabled={sessionIndex <= 0}
-          onClick={() => {
-            setSessionIndex((index) => index - 1);
-            resetScroll();
-          }}
-        >
-          <FontAwesomeIcon icon={faAngleLeft} />
-        </button>
-        <input
-          value={sessions[sessionIndex].name}
-          onChange={({ target: { value } }) => changeSessionName(value)}
-        />
-        <button
-          className={`px-3 ${
-            sessionIndex + 1 < sessions.length ? '' : 'text-gray-400'
-          }`}
-          disabled={sessionIndex + 1 >= sessions.length}
-          onClick={() => {
-            setSessionIndex((index) => index + 1);
-            resetScroll();
-          }}
-        >
-          <FontAwesomeIcon icon={faAngleRight} />
-        </button>
-        <button
-          className="px-2 text-white bg-gray-400 rounded"
-          onClick={addSession}
-        >
-          <FontAwesomeIcon icon={faPlus} />
-        </button>
-      </div>
-      <div>
-        <button className="px-2" onClick={onOpenButtonClick}>
-          <FontAwesomeIcon icon={open ? faAngleDown : faAngleUp} />
-        </button>
-      </div>
+    <div tw="sticky top-0 bg-white flex justify-between">
+      {left}
+      {right}
     </div>
   );
 };
