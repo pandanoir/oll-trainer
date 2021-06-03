0b385c959fc53094cad17cd8b2bb0f51bb76a5ba
pandanoir <moon_in_france@yahoo.co.jp>
2021-06-04 02:20:23 +0900

    Add TweetButton

---
+++ src/components/TweetButton.tsx
@@ -0,0 +1,23 @@
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import tw from 'twin.macro';
+
+export const TweetButton = ({ text }: { text: string }) => {
+  return (
+    <a
+      className="twitter-share-button"
+      css={[
+        tw`inline-block px-2 rounded border-b`,
+        tw`bg-blue-300 text-white border-blue-400`,
+        tw`dark:bg-blue-600 dark:text-white dark:border-blue-800`,
+      ]}
+      href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+        text
+      )}&url=%20`}
+      target="_blank"
+      rel="noreferrer"
+    >
+      <FontAwesomeIcon icon={faTwitter} /> Tweet
+    </a>
+  );
+};
