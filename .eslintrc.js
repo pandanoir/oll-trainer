d9921553fbee9b2707ddbe71b7f2745d86586040
pandanoir <moon_in_france@yahoo.co.jp>
2021-08-22 17:42:08 +0900

    Add JS config to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,50 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
-    'prettier/react',
-  ],
-  plugins: ['formatjs'],
+  extends: ['eslint:recommended', 'prettier'],
   env: {
-    browser: true,
+    node: true,
   },
   parserOptions: {
-    ecmaVersion: 6,
-    project: './tsconfig.json',
+    ecmaVersion: 2021,
   },
-  rules: {
-    'react/prop-types': 'off',
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'no-empty': ['error', { allowEmptyCatch: true }],
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'import/order': [
-      'error',
-      {
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
+  overrides: [
+    {
+      files: ['*.ts'],
+      extends: [
+        'eslint:recommended',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'prettier',
+        'prettier/react',
+      ],
+      plugins: ['formatjs'],
+      env: {
+        browser: true,
       },
-    ],
-    'formatjs/enforce-id': 'error',
-  },
+      parserOptions: {
+        ecmaVersion: 6,
+        project: './tsconfig.json',
+      },
+      rules: {
+        'react/prop-types': 'off',
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'no-empty': ['error', { allowEmptyCatch: true }],
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'import/order': [
+          'error',
+          {
+            alphabetize: {
+              order: 'asc',
+              caseInsensitive: true,
+            },
+          },
+        ],
+        'formatjs/enforce-id': 'error',
+      },
+    },
+  ],
 };
