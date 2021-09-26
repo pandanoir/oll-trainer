0e3ad906cbe0dc251497309476b7c595c77e327f
pandanoir <moon_in_france@yahoo.co.jp>
2021-09-26 18:12:25 +0900

    Change button bg to gradient

--- src/components/common/DangerButton.tsx
+++ src/components/common/DangerButton.tsx
@@ -1,6 +1,9 @@
 import tw from 'twin.macro';
 import { Button } from './Button';
 
-export const DangerButton = tw(
-  Button
-)`px-10 py-1.5 border-pink-400 bg-pink-600 text-white rounded dark:bg-pink-700 dark:border-pink-600`;
+export const DangerButton = tw(Button)`px-10 py-1.5
+border-pink-400 dark:border-pink-600
+bg-pink-600 dark:bg-pink-700
+bg-gradient-to-r from-pink-500 to-pink-600 dark:from-pink-600 dark:to-pink-700
+text-white rounded
+`;
