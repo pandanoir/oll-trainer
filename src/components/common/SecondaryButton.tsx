20e20681a096261736047cb1a205154cb1fd2e4b
pandanoir <moon_in_france@yahoo.co.jp>
2022-02-11 23:25:34 +0900

    Add disabled style to secondary buttons

--- src/components/common/SecondaryButton.tsx
+++ src/components/common/SecondaryButton.tsx
@@ -2,7 +2,9 @@ import { memo } from 'react';
 import tw from 'twin.macro';
 import { Button } from './Button';
 
-const SecondaryButtonRaw = tw(
-  Button
-)`border-2 border-blue-400 text-blue-600 bg-transparent rounded-md font-bold dark:border-blue-400 dark:text-blue-300`;
+const SecondaryButtonRaw = tw(Button)`
+border-2 border-blue-400 text-blue-600 bg-transparent rounded-md font-bold
+dark:border-blue-400 dark:text-blue-300
+disabled:border-gray-300 disabled:text-gray-400 disabled:cursor-default
+disabled:dark:border-gray-600 disabled:dark:text-gray-500`;
 export const SecondaryButton = memo(SecondaryButtonRaw);
--- src/components/common/SecondaryDangerButton.tsx
+++ src/components/common/SecondaryDangerButton.tsx
@@ -3,4 +3,7 @@ import { Button } from './Button';
 
 export const SecondaryDangerButton = tw(
   Button
-)`border-2 border-pink-400 text-pink-600 font-bold rounded dark:border-pink-600 dark:text-pink-500`;
+)`border-2 border-pink-400 text-pink-600 font-bold rounded
+dark:border-pink-600 dark:text-pink-500
+disabled:border-gray-300 disabled:text-gray-400 disabled:cursor-default
+disabled:dark:border-gray-600 disabled:dark:text-gray-500`;
