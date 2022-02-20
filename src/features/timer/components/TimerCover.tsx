b398d2682f9490da7956ed38e17e69b238087569
pandanoir <moon_in_france@yahoo.co.jp>
2022-02-20 10:01:27 +0900

    Add missing prop

--- src/features/timer/components/TimerCover.tsx
+++ src/features/timer/components/TimerCover.tsx
@@ -11,12 +11,16 @@ const TimerCoverRaw: VFC<{
   onPointerUp: (
     event: TouchEvent<HTMLDivElement> | MouseEvent<HTMLDivElement>
   ) => void;
+  onPointerCancel: (
+    event: TouchEvent<HTMLDivElement> | MouseEvent<HTMLDivElement>
+  ) => void;
   disabled?: boolean;
   transparent?: boolean;
   pressed: boolean;
 }> = ({
   onPointerDown,
   onPointerUp,
+  onPointerCancel,
   disabled = false,
   transparent = false,
   pressed,
@@ -26,6 +30,9 @@ const TimerCoverRaw: VFC<{
       onTouchStart={disabled ? undefined : withStopPropagation(onPointerDown)}
       onTouchMove={disabled ? undefined : withStopPropagation(noop)}
       onTouchEnd={disabled ? undefined : withStopPropagation(onPointerUp)}
+      onTouchCancel={
+        disabled ? undefined : withStopPropagation(onPointerCancel)
+      }
       onMouseDown={disabled ? undefined : withStopPropagation(onPointerDown)}
       onMouseUp={disabled ? undefined : withStopPropagation(onPointerUp)}
       ref={usePreventDefault('touchstart', !disabled)}
