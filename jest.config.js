0025a848fb41db32a3a4eb5550151fc81769cfc8
pandanoir <moon_in_france@yahoo.co.jp>
2022-04-03 13:25:35 +0900

    Add snapshot of TimerPage

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   testEnvironment: 'node',
-  collectCoverageFrom: ['src/**/*.ts?(x)'],
+  collectCoverageFrom: ['src/**/*.ts?(x)', '!**/*.guard.ts', '!**/*.d.ts'],
   setupFiles: ['./jest.setup.ts'],
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
   moduleNameMapper: {
     'swiper/react': 'swiper/react/swiper-react.js',
+    '\\.?worker': '<rootDir>/src/worker/recordWorker.ts',
   },
   transformIgnorePatterns: ['node_modules/(?!swiper|ssr-window|dom7)'],
 };
