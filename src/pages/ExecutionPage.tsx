a0a60f572203bbb4d406f42199a6af8648b7f069
pandanoir <moon_in_france@yahoo.co.jp>
2022-04-30 15:41:48 +0900

    Rename

--- src/pages/ExecutionPage.tsx
+++ src/pages/ExecutionPage.tsx
@@ -18,28 +18,28 @@ import { useInputWithStorage } from '../utils/hooks/useInput';
 import { useStoragedState } from '../utils/hooks/useLocalStorage';
 import { withPrefix } from '../utils/withPrefix';
 
-const Cubelet = tw.div`w-8 h-8 text-center align-top text-lg border-r border-b border-gray-800`;
-const WhiteCubelet = styled(Cubelet)(
+const BaseCubelet = tw.div`w-8 h-8 text-center align-top text-lg border-r border-b border-gray-800`;
+const WhiteCubelet = styled(BaseCubelet)(
   ({ selected = false }: { selected?: boolean }) =>
     selected ? tw`bg-gray-300 text-black` : tw`bg-gray-50 text-black`
 );
-const GreenCubelet = styled(Cubelet)(
+const GreenCubelet = styled(BaseCubelet)(
   ({ selected = false }: { selected?: boolean }) =>
     selected ? tw`bg-green-600 text-black` : tw`bg-green-500 text-black`
 );
-const RedCubelet = styled(Cubelet)(
+const RedCubelet = styled(BaseCubelet)(
   ({ selected = false }: { selected?: boolean }) =>
     selected ? tw`bg-red-700` : tw`bg-red-600`
 );
-const BlueCubelet = styled(Cubelet)(
+const BlueCubelet = styled(BaseCubelet)(
   ({ selected = false }: { selected?: boolean }) =>
     selected ? tw`bg-blue-600` : tw`bg-blue-500`
 );
-const OrangeCubelet = styled(Cubelet)(
+const OrangeCubelet = styled(BaseCubelet)(
   ({ selected = false }: { selected?: boolean }) =>
     selected ? tw`bg-orange-700` : tw`bg-orange-500`
 );
-const YellowCubelet = styled(Cubelet)(
+const YellowCubelet = styled(BaseCubelet)(
   ({ selected = false }: { selected?: boolean }) =>
     selected ? tw`bg-yellow-500 text-black` : tw`bg-yellow-300 text-black`
 );
